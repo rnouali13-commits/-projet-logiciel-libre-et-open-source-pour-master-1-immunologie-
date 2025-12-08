4755f654815d58fb82d736245a7d808ad7db844b
rnouali13-commits <r.nouali.13@gmail.com>
2025-12-08 22:25:05 +0100

    Add files via upload

--- Master1 immunologie_chef du projet_Nouali Rachad Abdelhafid .py
+++ Master1 immunologie_chef du projet_Nouali Rachad Abdelhafid .py
@@ -1,1 +1,13 @@
 
+#le chef de groupe: Nouali Rachad Abdelhafid /master1 immunologie/la date:06/12/2025
+#REGUIEG ISSAAD Chaimaa 
+#BERRANI AMINA 
+#METAIR CHAIMAA
+
+
+import pandas as pd
+data = {
+    "Séquence":["ATGCGTACGTA","GCTAGCTAGGCC","ATGCGCGTAAGT","TACGATCGTA","ATGAAAGGCTT","CGTACGTAGC","TTAACCGGAT"],
+    "longueur":[12,12,12,10,11,10,10],
+    "pourcentage GC":[50,66.67,58.33,40,45.45,60,50],
+}
