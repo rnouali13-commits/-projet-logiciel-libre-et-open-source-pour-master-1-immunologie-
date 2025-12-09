0be13d7f4854714296be2e0296d835037c11aca0
CHEKROUN Alaa Sara <alaasara2026@gmail.com>
2025-12-09 20:57:32 +0100

    Restore comments and formatting in Python script

--- Master1 immunologie_chef du projet_Nouali Rachad Abdelhafid .py
+++ Master1 immunologie_chef du projet_Nouali Rachad Abdelhafid .py
@@ -14,10 +14,11 @@ data = {
     "longueur":[12,12,12,10,11,10,10],
     "pourcentage GC":[50,66.67,58.33,40,45.45,60,50],
 }
-#Creation d'un dataFram (Tbleau pandas)
 
+#Creation d'un dataFram (Tableau pandas)
 tableau= pd.DataFrame(data)
-print(" création et affichage")
+print(" ***************création et affichage***************")
+
 # Affichage du tableau 
 print("Tableau des séquences ADN :" )
 print(tableau)
@@ -43,6 +44,7 @@ print(tableau)
 print("5/***l'ajoute d'une nouvelle colonne de catégorie GC***")
 tableau["catégorie GC"]=tableau["pourcentage GC"].apply(lambda x:"Riche" if x > 55 else("Moyen" if 45<= x <= 55 else "Faible" ))
 print(tableau,"\n")
+
 
 
 
